7cffdaf92c00c25637604d3d3ec68f9deeb04040
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-17 13:32:19 -0300

    feat: protected route

---
+++ src/utils/ProtectedRoute.tsx
@@ -0,0 +1,41 @@
+import { ReactNode, useEffect, useState } from "react";
+import { Navigate } from "react-router-dom";
+
+import { useUser } from "../contexts/UserContext";
+
+import Splash from "../pages/Splash";
+
+function ProtectedRoute({ children }: { children: ReactNode }) {
+  const { token, request } = useUser();
+  const [isLoading, setIsLoading] = useState(true);
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const verifyToken = async () => {
+    if (!token) {
+      setIsAuthenticated(false);
+      setIsLoading(false);
+      return;
+    }
+
+    try {
+      await request({ url: "/auth/verify", method: "GET" }, true);
+      setIsAuthenticated(true);
+    } catch (error) {
+      console.error("invalid token:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    verifyToken();
+  }, [token, request]);
+
+  if (isLoading) {
+    return <Splash />;
+  }
+
+  return isAuthenticated ? <>{children}</> : <Navigate to="/welcome" />;
+}
+
+export default ProtectedRoute;
