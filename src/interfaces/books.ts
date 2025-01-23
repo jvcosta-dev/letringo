4bec204cd0ff1e613c51ba2167829b748f932f0c
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-23 16:57:26 -0300

    interfaces

---
+++ src/interfaces/books.ts
@@ -0,0 +1,12 @@
+export interface Book {
+  id: number;
+  genre: string;
+  name: string;
+  description: string;
+  author: string;
+  thumb_url: string;
+  publisher: string;
+  src_url: string;
+  launch_date: string;
+  total_pages: number;
+}
---
+++ src/interfaces/stat.ts
@@ -0,0 +1,5 @@
+export interface Stat {
+  date: string;
+  pages_count: number;
+  read_time: number;
+}
