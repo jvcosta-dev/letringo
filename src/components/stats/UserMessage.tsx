45e1950d46ab26efeb4e448dcf10cea6381bcafc
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-23 16:59:03 -0300

    feat: home page

---
+++ src/components/stats/UserMessage.tsx
@@ -0,0 +1,29 @@
+import { FunctionComponent } from "react";
+import useSWR from "swr";
+
+import { useUser } from "../../contexts/UserContext";
+import MainTitle from "../ui/MainTitle";
+import Error from "../ui/Error";
+import Streak from "./Streak";
+import Loading from "../ui/Loading";
+
+interface UserMessageProps {}
+
+const UserMessage: FunctionComponent<UserMessageProps> = () => {
+  const { fetcher } = useUser();
+
+  const { data, isLoading, error } = useSWR(`/auth/me`, fetcher);
+  if (isLoading) return <Loading />;
+  if (error) {
+    return <Error code={error.code} />;
+  }
+
+  return (
+    <section className="w-full flex items-center justify-between">
+      <MainTitle start>Olá @{data.display_name}!</MainTitle>
+      <Streak />
+    </section>
+  );
+};
+
+export default UserMessage;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,45 +1,15 @@
 import Page from "../components/layout/Page";
+
 import ActiveRuns from "../components/run/ActiveRuns";
+import MonthlyStats from "../components/stats/MonthlyStats";
+import UserMessage from "../components/stats/UserMessage";
 
 function Home() {
   return (
-    <Page isInLayout exitDirection="right" noDelay>
-      <ActiveRuns
-        runs={[
-          {
-            id: 1,
-            user_id: 101,
-            book_id: 201,
-            goal: 320,
-            pages_count: 300,
-            read_time: 12,
-            finish_date: "2025-01-15",
-            page_list: "1-50,51-100,101-150,151-200,201-250,251-300",
-            book: {
-              name: "O Alquimista",
-              author: "Paulo Coelho",
-              thumb_url:
-                "https://m.media-amazon.com/images/I/81slUinjTlS._AC_UF1000,1000_QL80_.jpg",
-            },
-          },
-          {
-            id: 2,
-            user_id: 102,
-            book_id: 202,
-            goal: 200,
-            pages_count: 10,
-            read_time: 8,
-            finish_date: "2025-01-20",
-            page_list: "1-30,31-60,61-90,91-120,121-150,151-180,181-210",
-            book: {
-              name: "Dom Casmurro",
-              author: "Machado de Assis",
-              thumb_url:
-                "https://m.media-amazon.com/images/I/61Z2bMhGicL.jpghttps://m.media-amazon.com/images/I/61Z2bMhGicL.jpg",
-            },
-          },
-        ]}
-      />
+    <Page isInLayout instant>
+      <UserMessage />
+      <ActiveRuns />
+      <MonthlyStats />
     </Page>
   );
 }
