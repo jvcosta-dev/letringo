701dc3a56a33f155159263bf11da19591771d146
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-23 16:58:17 -0300

    active runs component

--- src/components/run/ActiveRuns.tsx
+++ src/components/run/ActiveRuns.tsx
@@ -1,21 +1,34 @@
 import { FunctionComponent } from "react";
+import useSWR from "swr";
+
 import Section from "../layout/Section";
 import { Run } from "../../interfaces/run";
 import RunCard from "./RunCard";
+import { useUser } from "../../contexts/UserContext";
+import Loading from "../ui/Loading";
+import Error from "../ui/Error";
 
-interface ActiveRunsProps {
-  runs: Run[];
-}
+interface ActiveRunsProps {}
 
-const ActiveRuns: FunctionComponent<ActiveRunsProps> = ({ runs }) => {
+const ActiveRuns: FunctionComponent<ActiveRunsProps> = ({}) => {
+  const { fetcher } = useUser();
+
+  const { data, isLoading, error } = useSWR(`/run/active`, fetcher);
+  if (isLoading) return <Loading />;
+  if (error) return <Error code={error.code} />;
+
   return (
-    <Section name="Continue Lendo">
-      <div className="flex flex-col gap-3">
-        {runs.map((run) => (
-          <RunCard run={run} key={run.id} />
-        ))}
-      </div>
-    </Section>
+    <>
+      {data && data.runs.length >= 1 && (
+        <Section name="Continue Lendo">
+          <div className="flex flex-col gap-3">
+            {data.runs.map((run: Run) => (
+              <RunCard run={run} key={run.id} />
+            ))}
+          </div>
+        </Section>
+      )}
+    </>
   );
 };
 
