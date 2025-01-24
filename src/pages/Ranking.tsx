b5e9db630c198730c863154748ec001dc3dc621f
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-24 15:03:16 -0300

    feat: ranking under dev

---
+++ src/pages/Ranking.tsx
@@ -0,0 +1,19 @@
+import { EngineeringRounded } from "@mui/icons-material";
+import Page from "../components/layout/Page";
+import MainTitle from "../components/ui/MainTitle";
+
+function Ranking() {
+  return (
+    <Page isInLayout instant>
+      <div className="flex flex-col flex-grow items-center justify-center">
+        <EngineeringRounded
+          className="text-primary"
+          style={{ width: 144, height: 144 }}
+        />
+        <MainTitle>Em desenvolvimento...</MainTitle>
+      </div>
+    </Page>
+  );
+}
+
+export default Ranking;
