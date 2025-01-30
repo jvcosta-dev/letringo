63f916ced35f879c7e39eaada37a9bd118688fb3
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-30 17:20:49 -0300

    fix css typo

--- src/components/layout/Page.tsx
+++ src/components/layout/Page.tsx
@@ -9,7 +9,7 @@ interface PageProps {
 const Page: FunctionComponent<PageProps> = ({ children, isInLayout }) => {
   return (
     <motion.main
-      className={`flex flex-grow h-full flex-col gap-5 px-6 pt-6 pb-6 md:px-32 lg:px-52 xl:px-[512px] ${
+      className={`flex flex-grow h-full flex-col gap-5 px-6 py-6 md:px-32 lg:px-52 xl:px-[512px] ${
         isInLayout
           ? "overflow-y-scroll"
           : "w-screen h-screen text-neutral-dark dark:bg-black dark:text-white"
