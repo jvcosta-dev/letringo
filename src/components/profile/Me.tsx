781cbb7f01fbe1821a816471d00d86cf31d5b2f9
jvcosta-dev <victor.matos.costa.dev@gmail.com>
2025-01-30 13:50:38 -0300

    feat: me component for better modularization

---
+++ src/components/profile/Me.tsx
@@ -0,0 +1,48 @@
+import { PersonRounded, SettingsRounded } from "@mui/icons-material";
+import { FunctionComponent } from "react";
+import { Link } from "react-router-dom";
+import Button from "../ui/Button";
+import useSWR from "swr";
+import { useUser } from "../../contexts/UserContext";
+import Loading from "../ui/Loading";
+import Error from "../ui/Error";
+
+interface MeProps {}
+
+const Me: FunctionComponent<MeProps> = () => {
+  const { fetcher } = useUser();
+
+  const { data, isLoading, error } = useSWR(`/auth/me`, fetcher);
+  if (isLoading) return <Loading />;
+  if (error) {
+    return <Error code={error.code} />;
+  }
+  return (
+    <>
+      <div className="flex items-center justify-center">
+        {data.avatar_url ? (
+          <img
+            src={data.avatar_url}
+            alt="foto de perfl"
+            width={144}
+            height={144}
+            className="rounded-full"
+          />
+        ) : (
+          <PersonRounded
+            style={{ width: 144, height: 144 }}
+            className="bg-primary rounded-full text-white"
+          />
+        )}
+      </div>
+      <div className="flex flex-col">
+        <h1 className="text-xl font-bold">@{data.display_name}</h1>
+        <p>
+          Membro desde {new Date(data.createdAt).toLocaleDateString("pt-BR")}
+        </p>
+      </div>
+    </>
+  );
+};
+
+export default Me;
--- src/pages/Profile.tsx
+++ src/pages/Profile.tsx
@@ -1,22 +1,12 @@
-import useSWR from "swr";
-import { PersonRounded, SettingsRounded } from "@mui/icons-material";
-
-import Page from "../components/layout/Page";
-import { useUser } from "../contexts/UserContext";
-import Loading from "../components/ui/Loading";
-import Error from "../components/ui/Error";
 import { Link } from "react-router-dom";
-import Button from "../components/ui/Button";
+import Page from "../components/layout/Page";
+import { SettingsRounded } from "@mui/icons-material";
+
+import Me from "../components/profile/Me";
 import AllStats from "../components/stats/AllStats";
+import Button from "../components/ui/Button";
 
 function Profile() {
-  const { fetcher } = useUser();
-
-  const { data, isLoading, error } = useSWR(`/auth/me`, fetcher);
-  if (isLoading) return <Loading />;
-  if (error) {
-    return <Error code={error.code} />;
-  }
   return (
     <Page isInLayout>
       <div className="flex justify-end">
@@ -26,28 +16,7 @@ function Profile() {
           </Button>
         </Link>
       </div>
-      <div className="flex items-center justify-center">
-        {data.avatar_url ? (
-          <img
-            src={data.avatar_url}
-            alt="foto de perfl"
-            width={144}
-            height={144}
-            className="rounded-full"
-          />
-        ) : (
-          <PersonRounded
-            style={{ width: 144, height: 144 }}
-            className="bg-primary rounded-full text-white"
-          />
-        )}
-      </div>
-      <div className="flex flex-col">
-        <h1 className="text-xl font-bold">@{data.display_name}</h1>
-        <p>
-          Membro desde {new Date(data.createdAt).toLocaleDateString("pt-BR")}
-        </p>
-      </div>
+      <Me />
       <AllStats />
     </Page>
   );
